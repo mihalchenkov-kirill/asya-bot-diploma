d0054d2dfa846d753ec74a3b22aca4d2a8dafb73
k.mikhalchenkov <k.mikhalchenkov@yadro.com>
2024-04-21 19:44:55 +0300

    Update asya-bot-diploma

--- handlers/menu_handler_router.py
+++ handlers/menu_handler_router.py
@@ -2,14 +2,14 @@ from aiogram import F, Router, types
 from aiogram.filters import CommandStart
 
 from constans.inline_ready_buttons import MAIN_MENU, RETURN_TO_MENU
-from placeholders.texts import text
+from placeholders.texts import main_text
 
 menu_handler_router = Router()
 
 
 @menu_handler_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer(text.welcome_text.format(user_name=message.from_user.first_name), reply_markup=MAIN_MENU)
+    await message.answer(main_text.welcome_text.format(user_name=message.from_user.first_name), reply_markup=MAIN_MENU)
 
 
 @menu_handler_router.callback_query(F.data == 'about_bot')
--- handlers/show_info_handler.py
+++ handlers/show_info_handler.py
@@ -2,7 +2,7 @@ from aiogram import F, Router, types
 
 from constans.inline_ready_buttons import YES_OR_NO
 from kbds.inline import get_callback_buttons
-from placeholders.texts import text
+from placeholders.texts import show_info_text
 
 MYSTERY_YES = get_callback_buttons(buttons={'Давай!': 'mystery_yes', 'Нет!': 'back_to_menu'})
 AFTER_MYSTERY_YES = get_callback_buttons(buttons={'Давай!': 'after_mystery_yes', 'Нет!': 'back_to_menu'})
@@ -22,48 +22,48 @@ show_info_handler_router = Router()
 
 @show_info_handler_router.callback_query(F.data == 'show_info')
 async def send_info(callback: types.CallbackQuery):
-    await callback.message.edit_text('Моя цель помочь. Соглы?', reply_markup=YES_OR_NO)
+    await callback.message.edit_text(show_info_text.mystery_1, reply_markup=YES_OR_NO)
 
 
 @show_info_handler_router.callback_query(F.data == 'answer_yes')
 async def step_one(callback: types.CallbackQuery):
-    await callback.message.edit_text('Загадка...! Соглы на загадку?', reply_markup=MYSTERY_YES)
+    await callback.message.edit_text(show_info_text.mystery_2, reply_markup=MYSTERY_YES)
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_yes')
 async def step_two(callback: types.CallbackQuery):
-    await callback.message.edit_text(text.mystery, reply_markup=MYSTERY_ANSWERS)
+    await callback.message.edit_text(show_info_text.mystery, reply_markup=MYSTERY_ANSWERS)
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_bad_choice_instinct')
 async def answer_instinct(callback: types.CallbackQuery):
-    await callback.message.answer('Инстинкт? Нет!')
+    await callback.message.answer(show_info_text.mystery_3)
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_bad_choice_wolf')
 async def answer_wolf(callback: types.CallbackQuery):
-    await callback.message.answer('Волк? Нет!')
+    await callback.message.answer(show_info_text.mystery_4)
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_bad_choice_sad')
 async def answer_sad(callback: types.CallbackQuery):
-    await callback.message.answer('Плохое настроение? Нет!')
+    await callback.message.answer(show_info_text.mystery_5)
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_bad_choice_emotion')
 async def answer_emotion(callback: types.CallbackQuery):
-    await callback.message.answer('Эмоции? Нет!')
+    await callback.message.answer(show_info_text.mystery_6)
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_right_choice')
 async def answer_right(callback: types.CallbackQuery):
-    await callback.message.answer('Мысли? Да! Хош расскажу?', reply_markup=AFTER_MYSTERY_YES)
+    await callback.message.answer(show_info_text.mystery_7, reply_markup=AFTER_MYSTERY_YES)
 
 
 @show_info_handler_router.callback_query(F.data == 'after_mystery_yes')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Мысль - это наиболее заметный сознанию',
+        show_info_text.mystery_8,
         reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_1'}),
     )
 
@@ -71,7 +71,7 @@ async def _(callback: types.CallbackQuery):
 @show_info_handler_router.callback_query(F.data == 'mystery_next_1')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Когнитивно-поведенческая терапия - это одно из самых популярных нап',
+        show_info_text.mystery_9,
         reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_2'}),
     )
 
@@ -79,7 +79,7 @@ async def _(callback: types.CallbackQuery):
 @show_info_handler_router.callback_query(F.data == 'mystery_next_2')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Бот КПТ поможет вам справиться с плохим настроением, трудностями с ко',
+        show_info_text.mystery_10,
         reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_3'}),
     )
 
@@ -87,28 +87,28 @@ async def _(callback: types.CallbackQuery):
 @show_info_handler_router.callback_query(F.data == 'mystery_next_3')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Краткое описание бота КПТ', reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_4'})
+        show_info_text.mystery_11, reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_4'})
     )
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_next_4')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Второй шаг: ', reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_5'})
+        show_info_text.mystery_12, reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_5'})
     )
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_next_5')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Третий шаг:', reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_6'})
+        show_info_text.mystery_13, reply_markup=get_callback_buttons(buttons={'Далее!': 'mystery_next_6'})
     )
 
 
 @show_info_handler_router.callback_query(F.data == 'mystery_next_6')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Буду прям и честен с тобой, дорогой друг! Не один телеграм б',
+        show_info_text.mystery_14,
         reply_markup=get_callback_buttons(buttons={'Далее!': 'zzz'}),
     )
 
@@ -116,5 +116,5 @@ async def _(callback: types.CallbackQuery):
 @show_info_handler_router.callback_query(F.data == 'zzz')
 async def _(callback: types.CallbackQuery):
     await callback.message.edit_text(
-        'Вступление - краткий экскурс в КПТ ',
+        show_info_text.mystery_15,
     )
