4e1c3fb17ab6e6960833444e8e3008a94233af7b
k.mikhalchenkov <k.mikhalchenkov@yadro.com>
2024-04-21 17:34:29 +0300

    Update asya-bot-diploma

--- handlers/menu_handler_router.py
+++ handlers/menu_handler_router.py
@@ -1,7 +1,7 @@
 from aiogram import F, Router, types
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
-from constans.inline_ready_buttons import MAIN_MENU
+from constans.inline_ready_buttons import MAIN_MENU, RETURN_TO_MENU
 from placeholders import text
 
 menu_handler_router = Router()
@@ -10,6 +10,11 @@ menu_handler_router = Router()
 @menu_handler_router.message(CommandStart())
 async def start_cmd(message: types.Message):
     await message.answer(text.welcome_text.format(user_name=message.from_user.first_name), reply_markup=MAIN_MENU)
+
+
+@menu_handler_router.callback_query(F.data == 'about_bot')
+async def back_to_menu(callback: types.CallbackQuery):
+    await callback.message.edit_text('(с) Я бот - Ася.', reply_markup=RETURN_TO_MENU)
 
 
 @menu_handler_router.callback_query(F.data == 'back_to_menu')
