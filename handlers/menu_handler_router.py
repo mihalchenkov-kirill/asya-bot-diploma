8e43dd24db578e8dade7067ca7587e3576f6663d
k.mikhalchenkov <k.mikhalchenkov@yadro.com>
2024-04-26 16:28:35 +0300

    Update asya-bot-diploma

--- handlers/menu_handler_router.py
+++ handlers/menu_handler_router.py
@@ -15,15 +15,15 @@ async def start_cmd(message: types.Message):
     await message.answer_photo(
         photo=menu,
         caption=main_text.welcome_text.format(user_name=message.from_user.first_name),
-        reply_markup=MAIN_MENU,
+        reply_markup=MAIN_MENU
     )
 
 
 @menu_handler_router.callback_query(F.data == 'about_bot')
 async def about(callback: types.CallbackQuery):
-    await callback.message.edit_text('(с) Я бот - Ася.', reply_markup=RETURN_TO_MENU)
+    await callback.message.answer(text=main_text.about_bot, reply_markup=RETURN_TO_MENU)
 
 
 @menu_handler_router.callback_query(F.data == 'back_to_menu')
 async def back_to_menu(callback: types.CallbackQuery):
-    await callback.message.edit_text('Главное меню', reply_markup=MAIN_MENU)
+    await callback.message.answer(text='Главное меню', reply_markup=MAIN_MENU)
